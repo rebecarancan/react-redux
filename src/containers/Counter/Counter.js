8a55f445a9e7def6d0909c8abb973e76ac8e4914
Rebeca Rancan <rebecarancanrr@gmail.com>
2021-01-13 08:41:57 -0300

    Updating Arrays Immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -17,7 +17,7 @@ class Counter extends Component {
                 <button onClick={this.props.onStoreResult}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResults => (
-                        <li key={strResults.id} onClick={this.props.onDeleteResult}>{strResults.value}</li>
+                        <li key={strResults.id} onClick={() => this.props.onDeleteResult(strResults.id)}>{strResults.value}</li>
                     ))}
                 </ul>
             </div>
@@ -39,7 +39,7 @@ const mapDispatchToProps = dispatch => {
         onAddCounter: () => dispatch({type: 'ADD', val: 10}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15}),
         onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
-        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId: id})
     };
 };
 
